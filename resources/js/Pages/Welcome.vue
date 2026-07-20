3dbbf78854ec3af65d1d0b225be277bec21995ce
Leo Angelo Torres <leoangeloatorres00@gmail.com>
2026-07-20 10:22:24 +0800

    update the comment in deploy.yml and welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -33,9 +33,6 @@ function handleImageError() {
         <img id="background" class="absolute -left-20 top-0 max-w-[877px]"
             src="https://laravel.com/assets/img/welcome/background.svg" />
 
-        <p>Testing Update</p>
-
-        <p>Testing Update Again</p>
         <div
             class="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
